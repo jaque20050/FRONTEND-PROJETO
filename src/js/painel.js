22074c8a950781de57dbc46d2c0bc7a2c05f858a
jaque20050 <jaque20050@gmail.com>
2022-01-17 17:41:54 -0300

    Inciando JavaScript

---
+++ src/js/painel.js
@@ -0,0 +1,11 @@
+/*
+Quando clicar na seta para avançar temos que esconder 
+todas as imagens e mostrar a proxima imagem.
+
+A imagem atual começa com 0, adicionar contador
+
+ */
+
+//const imagens = document
+
+console.log(document);
