0f2b74258b3de9769bf59b7838b900920fd5add2
jaque20050 <jaque20050@gmail.com>
2022-01-18 15:30:19 -0300

    Quando chegar a 3, para a imagem

--- src/js/painel.js
+++ src/js/painel.js
@@ -6,7 +6,29 @@ A imagem atual começa com 0, adicionar contador
 
  */
 
-const imagens = document.querySelectorAll('.imagem-painel');
+const imagensPainel = document.querySelectorAll('.imagem-painel');
 const setaAvancar = document.getElementById('btn-avancar');
 const setaVoltar = document.getElementById('btn-voltar');
 let imagemAtual = 0;
+
+
+setaAvancar.addEventListener('click', function() { 
+
+    // Testa se o contador de imagemAtual é igual ao total de imagem
+    const totalDeImagens = imagensPainel.length -1;
+    if(imagemAtual === totalDeImagens){
+        console.log('não pode mais avançar');
+        return;
+    }
+    
+    imagemAtual++;
+
+    
+    imagensPainel.forEach(imagem => {
+        imagem.classList.remove('mostrar')
+    });
+
+
+    imagensPainel[imagemAtual].classList.add('mostrar');
+    
+})
