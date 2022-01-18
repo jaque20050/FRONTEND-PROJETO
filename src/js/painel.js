b0613b359f8fa0b2fda76ead4fa39d6ead7f1f4f
jaque20050 <jaque20050@gmail.com>
2022-01-18 15:56:38 -0300

    Conclusão do JS CardImagem

--- src/js/painel.js
+++ src/js/painel.js
@@ -12,23 +12,43 @@ const setaVoltar = document.getElementById('btn-voltar');
 let imagemAtual = 0;
 
 
+function esconderImagens(){
+    imagensPainel.forEach(imagem => {
+        imagem.classList.remove('mostrar')
+    });
+}
+
+function mostrarImagem(){
+    imagensPainel[imagemAtual].classList.add('mostrar');
+}
+
+
 setaAvancar.addEventListener('click', function() { 
 
     // Testa se o contador de imagemAtual é igual ao total de imagem
     const totalDeImagens = imagensPainel.length -1;
     if(imagemAtual === totalDeImagens){
-        console.log('não pode mais avançar');
+        //console.log('não pode mais avançar');
         return;
     }
     
     imagemAtual++;
 
-    
-    imagensPainel.forEach(imagem => {
-        imagem.classList.remove('mostrar')
-    });
+    esconderImagens();
 
-
-    imagensPainel[imagemAtual].classList.add('mostrar');
+    mostrarImagem();
     
+});
+
+setaVoltar.addEventListener('click', function() { 
+
+    if(imagemAtual === 0){
+        return;
+    }
+
+    imagemAtual --;
+
+    esconderImagens();
+
+    mostrarImagem();
 })
